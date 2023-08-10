74b127a4b52758fc5d1a05ded818adf9a1ff7fe7
cloudzeus <gkozyris@cloudzeus/eu>
2023-08-10 09:49:49 +0300

    new component ccm

---
+++ src/api/ccm-subscription/routes/ccm-subscription.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ccm-subscription router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ccm-subscription.ccm-subscription');
---
+++ src/api/ccm-subscription/services/ccm-subscription.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ccm-subscription service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ccm-subscription.ccm-subscription');
